c324f9a37d2f85778703fb0056fcd61a68a2dad2
reza <mardanireza58@gmail.com>
2023-11-27 23:29:43 +0330

    add all apps to config

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    'bike.apps.BikeConfig',
+    'manager.apps.ManagerConfig'
 ]
 
 MIDDLEWARE = [
