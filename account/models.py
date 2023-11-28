2f7227a9104f9f5ee446fea0ee49837cd8e49468
reza <mardanireza58@gmail.com>
2023-11-28 08:06:07 +0330

    add class users

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    ...
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
