fadd60aefd8962acafaa987993e743739e86fbe2
reza <mardanireza58@gmail.com>
2023-11-28 09:27:54 +0330

    write model for bike

--- bike/models.py
+++ bike/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Type(models.Model):
+    type_name = models.CharField(max_length=250)
+    type_slug = models.CharField(max_length=20)
+
+class Bike(models.Model):
+    price = models.IntegerField(verbose_name='نرخ اجاره ساعتی')
+    add_date = models.DateField(auto_now_add=True, verbose_name='تاریخ اضافه شدن')
+    status = models.BooleanField(default=False, verbose_name='اجاره داده شده')
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, verbose_name='نوع')
+
+    class Meta:
+        verbose_name = "دوچرخه"
+        verbose_name_plural = "دورچرخه ها"
+
+    def __str__(self):
+        return f"{self.id} - {self.status}"
